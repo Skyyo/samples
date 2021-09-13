a4a6c42078aba84e1c003068127ee3896589e8d3
agamula <andriy.gamula@israelit.pro>
2021-09-13 17:32:54 +0300

    measure native view performance from composeable

--- app/src/main/java/com/skyyo/samples/features/otp/OtpScreen.kt
+++ app/src/main/java/com/skyyo/samples/features/otp/OtpScreen.kt
@@ -1,29 +1,55 @@
 package com.skyyo.samples.features.otp
 
+import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusOrder
 import androidx.compose.ui.input.key.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.isDigitsOnly
+import androidx.core.widget.doAfterTextChanged
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.android.material.textfield.TextInputLayout
+import android.widget.LinearLayout
+import androidx.compose.ui.res.stringResource
+import com.google.android.material.textfield.TextInputEditText
+
 
 @Composable
 fun OtpScreen(otpViewModel: OtpViewModel = hiltViewModel()) {
+    val context = LocalContext.current
+
+    //TODO get native views from activity to not bound to this composeable scope
+    val textInputLayout1 = remember {
+        createTextInputLayout(context).apply {
+            editText?.doAfterTextChanged { otpViewModel.onOtp1Entered(it.toString()) }
+        }
+    }
+    val otp1Input by otpViewModel.otp1.collectAsState()
+    textInputLayout1.error = otp1Input.errorId?.let { stringResource(it) }
+
+    val textInputLayout2 = remember {
+        createTextInputLayout(context).apply {
+            editText?.doAfterTextChanged { otpViewModel.onOtp2Entered(it.toString()) }
+        }
+    }
+    val otp2Input by otpViewModel.otp2.collectAsState()
+    val otp2Error = otp2Input.errorId?.let { stringResource(it) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -37,10 +63,25 @@ fun OtpScreen(otpViewModel: OtpViewModel = hiltViewModel()) {
         // add viewModel + PD restoration. add customization options to
         // cover 90% of cases
         Otp(Modifier.align(Alignment.CenterHorizontally))
+        AndroidView(factory = { textInputLayout1 }, modifier = Modifier.fillMaxWidth())
+        AndroidView(factory = { textInputLayout2 }, modifier = Modifier.fillMaxWidth(), update = {
+            it.error = otp2Error
+        })
+
         Button(onClick = { }) {
             Text(text = "validate")
         }
     }
+}
+
+private fun createTextInputLayout(context: Context) = TextInputLayout(context).apply {
+    val editText = TextInputEditText(context)
+
+    val textInputLayoutParams = LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    )
+    addView(editText, textInputLayoutParams)
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
--- app/src/main/java/com/skyyo/samples/features/otp/OtpViewModel.kt
+++ app/src/main/java/com/skyyo/samples/features/otp/OtpViewModel.kt
@@ -1,8 +1,12 @@
 package com.skyyo.samples.features.otp
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.skyyo.samples.R
 import com.skyyo.samples.application.EditProfileGraph
+import com.skyyo.samples.extensions.getStateFlow
+import com.skyyo.samples.features.inputValidations.InputWrapper
 import com.skyyo.samples.utils.eventDispatchers.NavigationDispatcher
 import com.skyyo.samples.utils.eventDispatchers.UnauthorizedEventDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,7 +17,24 @@ import javax.inject.Inject
 class OtpViewModel @Inject constructor(
     private val navigationDispatcher: NavigationDispatcher,
     private val unauthorizedEventDispatcher: UnauthorizedEventDispatcher,
+    private val handle: SavedStateHandle,
 ) : ViewModel() {
+
+    val otp1 = handle.getStateFlow(viewModelScope, "otp1", InputWrapper())
+    val otp2 = handle.getStateFlow(viewModelScope, "otp2", InputWrapper())
+
+    fun onOtp1Entered(input: String) {
+        otp1.value = otp1.value.copy(value = input, errorId = input.validate())
+    }
+
+    fun onOtp2Entered(input: String) {
+        otp2.value = otp2.value.copy(value = input, errorId = input.validate())
+    }
+
+    private fun String.validate(): Int? = when (length) {
+        3 -> null
+        else -> R.string.validation_error
+    }
 
     fun onBtnClick() {
         navigationDispatcher.emit { it.navigate(EditProfileGraph.route) }
