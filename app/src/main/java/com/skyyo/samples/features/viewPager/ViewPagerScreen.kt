a454dccfbea8dba5258b4d5613e07f0e4d4553be
AndriyGoryachok <andriy1507@hotmail.com>
2021-09-13 22:48:50 +0300

    Custom pager indicator; Tabs animations;

--- app/src/main/java/com/skyyo/samples/features/viewPager/ViewPagerScreen.kt
+++ app/src/main/java/com/skyyo/samples/features/viewPager/ViewPagerScreen.kt
@@ -1,17 +1,22 @@
 package com.skyyo.samples.features.viewPager
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.snapshotFlow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.CarRepair
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
 import com.google.accompanist.insets.statusBarsPadding
@@ -24,35 +29,48 @@ import kotlin.math.absoluteValue
 @ExperimentalPagerApi
 @Composable
 fun ViewPagerScreen() {
-    val pagerState = rememberPagerState(pageCount = 10, initialOffscreenLimit = 2)
-    val scope = rememberCoroutineScope()
     val pages = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
+    val pagerState = rememberPagerState(pageCount = pages.size)
+    val scope = rememberCoroutineScope()
+//    val pagerState = rememberPagerState(
+//        pageCount = 10, // all others will be killed & recreated
+//        initialOffscreenLimit = 2, // to preload more items
+//    )
     LaunchedEffect(pagerState) {
         snapshotFlow { pagerState.currentPage }.collect { page ->
             log("current page $page")
         }
     }
-
-    Column {
+    val backgroundColor by animateColorAsState(
+        targetValue = androidx.compose.ui.graphics.lerp(
+            start = MaterialTheme.colors.primary,
+            stop = MaterialTheme.colors.secondary,
+            fraction = with(pagerState) { (currentPageOffset + currentPage) / pageCount }
+        ),
+        animationSpec = tween(durationMillis = 250, easing = LinearEasing)
+    )
+    Column(modifier = Modifier.statusBarsPadding()) {
         ScrollableTabRow(
-            edgePadding = 0.dp,
             selectedTabIndex = pagerState.currentPage,
             indicator = { tabPositions ->
                 TabRowDefaults.Indicator(
-                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+//                    color = Color.White,
+                    modifier = Modifier
+                        .pagerTabIndicatorOffset(pagerState, tabPositions)
+                        .clip(RoundedCornerShape(50)),
+                    height = 2.dp
                 )
-            }
+            },
+            backgroundColor = backgroundColor
         ) {
             // Add tabs for all of our pages
             pages.forEachIndexed { index, title ->
                 Tab(
-                    modifier = Modifier.statusBarsPadding(),
                     text = { Text("$title") },
+//                    selectedContentColor = Color.Yellow,
+//                    unselectedContentColor = Color.Green,
                     selected = pagerState.currentPage == index,
-                    onClick = {
-                        scope.launch { pagerState.animateScrollToPage(index) }
-                    },
+                    onClick = { scope.launch { pagerState.animateScrollToPage(index) } },
                 )
             }
         }
@@ -93,8 +111,8 @@ fun ViewPagerScreen() {
                         text = "Page: $page",
                         modifier = Modifier
                             .fillMaxWidth(0.5f)
-                            .height(100.dp)
-                            .background(Color.Cyan)
+                            .fillMaxHeight(0.5f)
+                            .background(backgroundColor)
                     )
                 }
             }
@@ -105,62 +123,28 @@ fun ViewPagerScreen() {
                 .align(Alignment.CenterHorizontally)
                 .padding(16.dp),
         )
-        Spacer(modifier = Modifier.height(20.dp))
-
-        HorizontalPagerWithOffset(itemWidth = 150.dp, offset = 20.dp)
-    }
-}
-
-@Composable
-@ExperimentalPagerApi
-fun HorizontalPagerWithOffset(
-    offset: Dp,
-    itemWidth: Dp,
-) {
-    BoxWithConstraints {
-        val pagerState = rememberPagerState(
-            pageCount = 10,
-            initialOffscreenLimit = (maxWidth / itemWidth).toInt() + 1
-        )
-
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.Start
-        ) { page ->
-            Card(Modifier.graphicsLayer {
-                // Calculate the absolute offset for the current page from the
-                // scroll position. We use the absolute value which allows us to mirror
-                // any effects for both directions
-                val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
-
-                lerp(
-                    start = 0.85f,
-                    stop = 1f,
-                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                ).also { scale ->
-                    scaleX = scale
-                    scaleY = scale
-                }
-
-                // We animate the alpha, between 50% and 100%
-                alpha = lerp(
-                    start = 0.5f,
-                    stop = 1f,
-                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
+        CustomHorizontalPagerIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp),
+            indicatorWidth = 16.dp,
+            activeIndicator = {
+                Image(
+                    modifier = Modifier.matchParentSize(),
+                    imageVector = Icons.Rounded.CarRepair,
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(Color.White)
                 )
-
-                //offset for snapping
-                translationX = offset.toPx()
-            }) {
-                Text(
-                    text = "Page: $page",
+            },
+            inactiveIndicator = {
+                Box(
                     modifier = Modifier
-                        .width(itemWidth)
-                        .height(100.dp)
-                        .background(Color.Cyan)
+                        .matchParentSize()
+                        .clip(RoundedCornerShape(2.dp))
+                        .background(backgroundColor)
                 )
             }
-        }
+        )
     }
 }
