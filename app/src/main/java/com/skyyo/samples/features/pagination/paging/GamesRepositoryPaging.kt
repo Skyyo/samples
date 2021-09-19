bf5c9523823ac1309724a7939e69b28408485c91
agamula <andriy.gamula@israelit.pro>
2021-09-19 14:24:02 +0300

    add pagination bug comment

--- app/src/main/java/com/skyyo/samples/features/pagination/paging/GamesRepositoryPaging.kt
+++ app/src/main/java/com/skyyo/samples/features/pagination/paging/GamesRepositoryPaging.kt
@@ -6,6 +6,9 @@ import com.skyyo.samples.application.network.calls.GamesCalls
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
+// TODO if pageSize is small, not setting initial load size will cause to crash.
+// Tested on emulator pixel 3a and motorola nexus 6.
+// Repro for pageSize <= 6. For pageSize = 7 repro on nexus 6 with fast scrolling bottom/top.
 private const val PAGE_LIMIT = 30
 
 @ViewModelScoped
