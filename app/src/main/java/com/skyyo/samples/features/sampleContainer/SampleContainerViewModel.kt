854aeacd193ebbbab48b66f812c50dfb2ae658bd
Rostyslav Holub <kloosprog@gmail.com>
2022-04-11 10:21:13 +0300

    add autoFillModifier

--- app/src/main/java/com/skyyo/samples/features/sampleContainer/SampleContainerViewModel.kt
+++ app/src/main/java/com/skyyo/samples/features/sampleContainer/SampleContainerViewModel.kt
@@ -176,4 +176,8 @@ class SampleContainerViewModel @Inject constructor(
     fun goPdfViewer() = navigationDispatcher.emit {
         it.navigate(Destination.PdfViewer.route)
     }
+
+    fun goAutoFill() = navigationDispatcher.emit {
+        it.navigate(Destination.AutoFill.route)
+    }
 }
