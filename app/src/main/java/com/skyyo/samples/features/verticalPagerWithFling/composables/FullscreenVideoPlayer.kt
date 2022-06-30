81fd2f49b00b1e4ffe787f62daf2b4150c168ec8
Andrii Hamula <andriy.gamula@israelit.pro>
2022-06-30 17:22:42 +0300

    minor improvement

--- app/src/main/java/com/skyyo/samples/features/verticalPagerWithFling/composables/FullscreenVideoPlayer.kt
+++ app/src/main/java/com/skyyo/samples/features/verticalPagerWithFling/composables/FullscreenVideoPlayer.kt
@@ -2,7 +2,7 @@ package com.skyyo.samples.features.verticalPagerWithFling.composables
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -22,8 +22,9 @@ fun FullscreenVideoPlayer(exoPlayer: ExoPlayer?) {
         }
     }
 
-    SideEffect {
+    DisposableEffect(exoPlayer) {
         playerView.player = exoPlayer
+        onDispose { playerView.player = null }
     }
 
     AndroidView(
